a3f43d5746099273f3733de6637b144715752105
Evan W. Patton <ewpatton@mit.edu>
2025-04-13 01:16:08 -0400

    Report results in index.html
    Change-Id: I2e17253b6dcebc63dee69cbaa5ea2e9e733ae131

--- appinventor/components/src/edu/mit/appinventor/ai/supersrc/com/google/appinventor/components/runtime/EventDispatcher.java
+++ appinventor/components/src/edu/mit/appinventor/ai/supersrc/com/google/appinventor/components/runtime/EventDispatcher.java
@@ -3,6 +3,13 @@ package com.google.appinventor.components.runtime;
 public class EventDispatcher {
   public native static boolean dispatchEvent(Component source, String eventName, Object... args) /*-{
     $wnd.console.log('Handling ' + eventName + ' for ' + source + ', args:', args);
-    return true;
+    if ($wnd.registeredEvents && $wnd.registeredEvents[eventName]) {
+      var event = $wnd.registeredEvents[eventName];
+      if (event) {
+        event.apply(source, args);
+        return true;
+      }
+    }
+    return false;
   }-*/;
 }
